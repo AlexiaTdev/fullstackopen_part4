cff7a9a8cb6f5af29a55bdd1e37a0baccf7a0bf8
AlexiaTdev <alexia.teissier1@epsi.fr>
2025-01-21 16:04:43 -0500

    part4 tests w express async errors for multiple promises

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -11,15 +11,22 @@ const Note = require('../models/note')
 const helper = require('./test_helper')
 
 beforeEach(async () => {
+//   await Note.deleteMany({})
+//   let noteObject = new Note(helper.initialNotes[0])
+//   await noteObject.save()
+//   noteObject = new Note(helper.initialNotes[1])
+//   await noteObject.save()
   await Note.deleteMany({})
-  let noteObject = new Note(helper.initialNotes[0])
-  await noteObject.save()
-  noteObject = new Note(helper.initialNotes[1])
-  await noteObject.save()
+
+  const noteObjects = helper.initialNotes
+    .map(note => new Note(note))
+  const promiseArray = noteObjects.map(note => note.save())
+  await Promise.all(promiseArray)
 })
 
 
 test.only('notes are returned as json', async () => {
+  console.log('entered test')
   await api
     .get('/api/notes')
     .expect(200)
