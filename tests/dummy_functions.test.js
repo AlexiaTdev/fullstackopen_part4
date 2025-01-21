078e050cf98acb600bee62f7a4a789607de6f185
AlexiaTdev <alexia.teissier1@epsi.fr>
2025-01-21 09:57:06 -0500

    part4 helper functions and unit tests - 4.5 step3

--- tests/dummy_functions.test.js
+++ tests/dummy_functions.test.js
@@ -3,62 +3,128 @@ const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
 describe('dummy', () => {
-    test('dummy returns one', () => {
+  test('dummy returns one', () => {
     const blogs = []
 
     const result = listHelper.dummy(blogs)
     assert.strictEqual(result, 1)
-    })
+  })
 })
 
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5,
+    __v: 0
+  }
+]
+const listWithMultipleBlog =[
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d12k2',
+    title: 'another example',
+    author: 'Mr Doe',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/mrDoe.pdf',
+    likes: 22,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d12m6',
+    title: 'again another example',
+    author: 'Mme Doe',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/mmeDoe.pdf',
+    likes: 63,
+    __v: 0
+  }
+]
+const listWithMultipleTopBlogs =[
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d12k2',
+    title: 'another example',
+    author: 'Mr Doe',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/mrDoe.pdf',
+    likes: 22,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d12m6',
+    title: 'again another example',
+    author: 'Mme Doe',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/mmeDoe.pdf',
+    likes: 63,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d19p3',
+    title: 'again another example TWIN',
+    author: 'Mme DoeTwin',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/mmeDoeTwin.pdf',
+    likes: 63,
+    __v: 0
+  }
+]
 describe('total likes', () => {
-    const listWithOneBlog = [
-      {
-        _id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-        likes: 5,
-        __v: 0
-      }
-    ]
-    
-    test('of empty list is zero', () => {
-        const result = listHelper.totalLikes([])
-        assert.strictEqual(result, 0)
-    })
-    test('when list has only one blog, equals the likes of that', () => {
-        const result = listHelper.totalLikes(listWithOneBlog)
-        assert.strictEqual(result, 5)
-    })
-    listWithMultipleBlog =[
-        {
-          _id: '5a422aa71b54a676234d17f8',
-          title: 'Go To Statement Considered Harmful',
-          author: 'Edsger W. Dijkstra',
-          url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-          likes: 5,
-          __v: 0
-        },
-        {
-            _id: '5a422aa71b54a676234d12k2',
-            title: 'another example',
-            author: 'Mr Doe',
-            url: 'https://homepages.cwi.nl/~storm/teaching/reader/mrDoe.pdf',
-            likes: 22,
-            __v: 0
-        },
-        {
-            _id: '5a422aa71b54a676234d12m6',
-            title: 'again another example',
-            author: 'Mme Doe',
-            url: 'https://homepages.cwi.nl/~storm/teaching/reader/mmeDoe.pdf',
-            likes: 63,
-            __v: 0
-        }
-    ] 
-    test('of a bigger list is calculated right', () => {
-      const result = listHelper.totalLikes(listWithMultipleBlog)
-      assert.strictEqual(result, 90)
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(listWithMultipleBlog)
+    assert.strictEqual(result, 90)
+  })
+}
+)
+
+describe('favorite blog', () => {
+  test('of empty list is empty blog', () => {
+    const result = listHelper.favoriteBlog([])
+    assert.deepStrictEqual(result, {
+      title: '',
+      author: '',
+      likes: 0
     })
   })
+  test('of a list gets right item', () => {
+    const result = listHelper.favoriteBlog(listWithMultipleBlog)
+    assert.deepStrictEqual(result,
+      {
+        title: 'again another example',
+        author: 'Mme Doe',
+        likes: 63
+      }
+    )
+  })
+  test('of a list with multiple tops gets right item', () => {
+    const result = listHelper.favoriteBlog(listWithMultipleTopBlogs)
+    assert.deepStrictEqual(result,
+      {
+        title: 'again another example',
+        author: 'Mme Doe',
+        likes: 63
+      }
+    )
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,13 +1,32 @@
 const dummy = (blogs) => {
-    return 1;
-  }
+  return 1
+}
 
 const totalLikes = (blogs) => {
-    var likes = blogs.reduce((acc, current)=> acc += current.likes, 0);
-    return likes;
+  var likes = blogs.reduce((acc, current) => acc += current.likes, 0)
+  return likes
 }
-  
-  module.exports = {
-    dummy,
-    totalLikes
+const favoriteBlog = (blogs) => {
+  var favorite = {
+    title: '',
+    author: '',
+    likes: 0
   }
+  blogs.forEach((blog) => {
+    if(blog.likes>favorite.likes){
+      favorite={
+        title: blog.title,
+        author: blog.author,
+        likes: blog.likes
+      }
+    }
+  }
+  )
+  return favorite
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog
+}
