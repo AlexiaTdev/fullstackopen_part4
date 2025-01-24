720baeea0ae494d388c20022bb397d0b1dd2e6d0
AlexiaTdev <alexia.teissier1@epsi.fr>
2025-01-24 15:42:45 -0500

    part4 blog list expansions- 4.17 step5

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,9 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({}).populate('notes', { content: 1, important: 1 })
+  const users = await User.find({})
+    .populate('notes', { content: 1, important: 1 })
+    .populate('blogs', { url: 1, title: 1, author: 1, id: 1 })
   response.json(users)
 })
 
